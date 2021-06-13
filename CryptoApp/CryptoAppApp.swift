0aeae1c59bb634a3b9534078d7af2a958363f025
らい <yuanleihachi@gmail.com>
2021-06-13 18:47:39 +0900

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by YUAN LEI on 2021/06/13.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
